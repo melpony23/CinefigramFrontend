7d43900f447b2ac5128f257422f60d1f2e63f0f6
vguinez <vguinez@uc.cl>
2024-05-31 14:48:46 -0400

    refactor: Cambio path de imagenes en Nosotros Page

--- src/pages/Nosotros.jsx
+++ src/pages/Nosotros.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import "./Nosotros.css"
 import {ImgLeft} from "../components/AboutUs-Cards/ImgLeft-Cards"
 import {ImgRight} from "../components/AboutUs-Cards/ImgRight-Cards"
-import fotoMel from "../assets/Foto_Mel.jpg"
-import fotoKate from "../assets/Foto_kate.jpg"
-import fotoVice from "../assets/Foto_vice.jpg"
+import fotoMel from "../../assets/Views/ViewsNosotrosPage/Foto_Mel.jpg"
+import fotoKate from "../../assets/Views/ViewsNosotrosPage/Foto_kate.jpg"
+import fotoVice from "../../assets/Views/ViewsNosotrosPage/Foto_vice.jpg"
 import logo from "../../assets/Logo_png.png"
 
 export const Nosotros = () => {
