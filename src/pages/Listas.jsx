7f8a074e7981fbfa066c5d979b6837330767af60
Katerina Reyes <katerina.reyes@uc.cl>
2024-06-09 20:23:45 -0400

    Add: background color view listas

--- src/pages/Listas.jsx
+++ src/pages/Listas.jsx
@@ -3,9 +3,19 @@ import "./Listas.css"
 
 export const Listas = () => {
     return (
-        <body>
-            <div>
-                <h1>Hola listas</h1>
+        <body className='Body_listas'>
+            <div className='Separador_listas'></div>
+            
+            <div className='content_listas'>
+                <div className='contenedor_titulo_listas'>
+                    <h1 className='titulo_listas'>
+                        Collect, curate, and share. Lists are the perfect way to group films.
+                    </h1>
+                    
+                </div>
+                <div className='contenedor_listas'>
+
+                    </div>
             </div>
         </body>
     );
