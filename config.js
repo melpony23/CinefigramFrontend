d239b43d8754d6dedfdc0c9fcfd86de02d9e6760
Katerina Reyes <katerina.reyes@uc.cl>
2024-06-14 18:24:40 -0400

    Fix: cambio url backend

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-const URL_BACK = 'http://localhost:3000/';
+const URL_BACK = 'postgres://amimir_database_user:fKMHgtdpYzUc2mhqlwpSfeTlm7FMTA8T@dpg-cpge6v0l6cac73b9c5d0-a.oregon-postgres.render.com/amimir_database';
 
 export default URL_BACK;
