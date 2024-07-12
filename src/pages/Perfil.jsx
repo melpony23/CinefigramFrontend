fe68ef8e3b795796cc20293b3a76f29af0b6b10b
Melanie Castillo <mmcastillo1@uc.cl>
2024-07-11 22:01:31 -0400

    fix: views perfiles

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,38 +1,22 @@
-import { useState, useEffect, useContext } from 'react';
-import { useParams, useNavigate} from 'react-router-dom'; // Importa navigate desde react-router-dom
+import { useState, useEffect } from 'react';
+import { useParams} from 'react-router-dom'; // Importa navigate desde react-router-dom
 import axios from 'axios';
-import { AuthContext } from '../auth/AuthContext';
 import './Perfil.css';
 import './Comunidad.css';
 import ListaGrande_Card from '../components/ListaGrande-Card/ListaGrande-Card';
-import Logro from '../components/Logro/Logro';
 import VITE_BACKEND_URL from "/config";
 import ReviewCard from '../components/ReviewCard/ReviewCard'; // Importar ReviewCard
 
-export const Perfil = () => {
+const PerfilId = () => {
+    const id = useParams().id;
     const [username, setUsername] = useState(null);
     const [fotoPerfil, setFotoPerfil] = useState(null);
-    const [descripcion, setDescripcion] = useState(null);
-    const { token } = useContext(AuthContext);
+    const [comments, setComments] = useState([]);
     const [logros, setLogros] = useState([]);
-    const [gotLogros, setGotLogros] = useState(false);
-    const { id } = useParams();
-    const navigate = useNavigate();
-    const [listas, setListas] = useState([]);
     const [gotListas, setGotListas] = useState(false);
-    const [reviewCount, setReviewCount] = useState(0);
+    const [descripcion, setDescripcion] = useState(null);
+    const [listas, setListas] = useState([]);
     const [reviews, setReviews] = useState([]);
-    const [gotReviews, setGotReviews] = useState(false);
-
-    useEffect(() => {
-        const storedUsername = localStorage.getItem('username');
-        const storedFotoPerfil = localStorage.getItem('fotoPerfil');
-        const storedDescripcion = localStorage.getItem('descripcion');
-
-        if (storedUsername) setUsername(storedUsername);
-        if (storedFotoPerfil) setFotoPerfil(storedFotoPerfil);
-        if (storedDescripcion) setDescripcion(storedDescripcion);
-    }, []);
 
     useEffect(() => {
         const config_get_listas = {
@@ -46,8 +30,10 @@ export const Perfil = () => {
         const getListas = async () => {
             if (!gotListas) {
                 try {
+
                     const response = await axios(config_get_listas);
                     setListas(response.data);
+                  
                     setGotListas(true);
                 } catch (error) {
                     console.log(error);
@@ -58,91 +44,74 @@ export const Perfil = () => {
     }, [id, gotListas]);
 
     useEffect(() => {
-        const config_get_logros = {
-            method: 'get',
-            url: `${VITE_BACKEND_URL}users/${id}/logros`,
-            headers: {
-                Authorization: `Bearer ${token}`,
-                'Content-Type': 'application/json'
-            },
-        };
-
-        const getLogros = async () => {
-            if (!gotLogros) {
-                try {
-                    const response = await axios(config_get_logros);
-                    setLogros(response.data);
-                    setGotLogros(true);
-                } catch (error) {
-                    console.log(error);
-                }
-            }
-        };
-        getLogros();
-    }, [id, gotLogros, token]);
-
-    useEffect(() => {
-        const config_get_review_count = {
-            method: 'get',
-            url: `${VITE_BACKEND_URL}reviews/usuario/${id}/count`,
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        };
-
-        const getReviewCount = async () => {
+        const fetchUserData = async () => {
             try {
-                const response = await axios(config_get_review_count);
-                setReviewCount(response.data.count);
+                const response = await axios.get(`${VITE_BACKEND_URL}userPublic/${id}`);
+                setUsername(response.data.username);
+                setFotoPerfil(response.data.fotoPerfil); // Asegúrate de que response.data.fotoPerfil sea una URL válida
+                setDescripcion(response.data.descripcion);
             } catch (error) {
                 console.log(error);
             }
         };
-        getReviewCount();
+
+        fetchUserData();
     }, [id]);
 
     useEffect(() => {
-        const config_get_reviews = {
-            method: 'get',
-            url: `${VITE_BACKEND_URL}reviews/usuario/${id}`,
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        };
-
-        const getReviews = async () => {
-            if (!gotReviews) {
-                try {
-                    const response = await axios(config_get_reviews);
-                    setReviews(response.data);
-                    setGotReviews(true);
-                } catch (error) {
-                    console.log(error);
-                }
+        const fetchReviews = async () => {
+            try {
+                const response = await axios.get(`${VITE_BACKEND_URL}reviews/usuario/${id}`);
+                setReviews(response.data);
+            } catch (error) {
+                console.log(error);
             }
         };
-        getReviews();
-    }, [id, gotReviews]);
 
-    const handleReviewClick = (movieId) => {
-        navigate(`/pelicula/${movieId}`);
-    };
+        fetchReviews();
+    }, [id]);
+
+    useEffect(() => {
+        const fetchComments = async () => {
+            try {
+                const response = await axios.get(`${VITE_BACKEND_URL}comments/usuario/${id}`);
+                setComments(response.data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        fetchComments();
+    }, [id]);
+
+    useEffect(() => {
+        const fetchLogros = async () => {
+            try {
+                const response = await axios.get(`${VITE_BACKEND_URL}logros/usuarios/${id}`);
+                setLogros(response.data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        fetchLogros();
+    }, [id]);
 
     const handleReviewDelete = async (reviewId) => {
-        const review = await axios.get(`${VITE_BACKEND_URL}reviews/${reviewId}`);
-        if (review.data.usuarioId != localStorage.getItem("userId")){
-            alert("Solo puedes eliminar Reviews tuyas");
-            return;
-        }
         try {
-            await axios.delete(`${VITE_BACKEND_URL}reviews/${reviewId}`);
-            setReviews(prevReviews => prevReviews.filter(review => review.id !== reviewId));
-            console.log('Review Eliminada:', reviewId);
-            window.location.reload();
+            const response = await axios.delete(`${VITE_BACKEND_URL}reviews/${reviewId}`);
+            if (response.status === 200) {
+                setReviews(prevReviews => prevReviews.filter(review => review.id !== reviewId));
+                console.log('Review Eliminada correctamente');
+            }
         } catch (error) {
-            console.log(error);
+            console.log('Error al eliminar la review:', error);
         }
-    }
+    };
+
+    const handleClickLogro = (logro) => {
+        alert(logro.descripcion);
+    };
 
     return (
         <div className='fondo-perfil'>
@@ -156,62 +125,68 @@ export const Perfil = () => {
                     <div className="info-usuario">
                         <h1>{username}</h1>
                         <div className="stats">
-                            <p>Reviews: {reviewCount}</p>
+                            <p>Reviews: {reviews.length}</p>
+                            <p>Comentarios: {comments.length}</p>
+                            <p>Logros: {logros.length}</p>
                         </div>
                         <br />
                         <div>
-                            <p>Descripción: {descripcion}</p>
+                            <p>{descripcion}</p>
                         </div>
                     </div>
                 </div>
             </div>
 
             <h4 className='font-custome-tittle card-title'>Logros de {username}</h4>
-            <div className='contenedor_logros'>
-                {logros.length === 0 ? (
-                    <h2>No tienes logros todavía</h2>
-                ) : (
-                    logros.map(logro => (
-                        <Logro key={logro.id} titulo={logro.titulo} logo={logro.logo} />
+            <div className='div-logros'>
+                {Array.isArray(logros) && logros.length > 0 ? (
+                    logros.map((logro, index) => (
+                        <div key={index} className='logro' onClick={() => handleClickLogro(logro)}>
+                            <img src={logro.logo} alt='Logro' />
+                            <p>{logro.titulo}</p>
+                        </div>
                     ))
+                ) : (
+                    <p>Este usuario todavía no ha ganado ningún logro.</p>
                 )}
             </div>
 
             <h4 className='font-custome-tittle card-title'>Reviews de {username}</h4>
-            <div className='contenedor_reviews'>
-                {reviews.length === 0 ? (
-                    <h2>No tienes reviews todavía. ¡Escribe una!</h2>
-                ) : (
-                    reviews.map(review => (
-                        review && review.id ? (
+            <div className='carrusel-reviews'>
+                <div className='div_contenedor_reviews2'>
+                    {Array.isArray(reviews) && reviews.length > 0 ? (
+                        reviews.map(review => (
                             <ReviewCard
                                 key={review.id}
                                 deletefunction={handleReviewDelete}
                                 id={review.id}
                                 movieId={review.peliculaId}
+                                fecha={review.fecha}
                                 userId={review.usuarioId}
                                 estado={review.estado}
                                 title={review.titulo}
                                 rating={review.calificacion}
                                 text={review.texto}
-                                fecha={review.fecha}
-                                clickfunction={handleReviewClick}
                             />
-                        ) : null
-                    ))
-                )}
+                        ))
+                    ) : (
+                        <p>Este usuario no ha hecho reviews.</p>
+                    )}
+                </div>
             </div>
 
             <h4 className='font-custome-tittle card-title'>Listas de {username}</h4>
-
-            <div >
-                {listas.length === 0 ? (<h2>No tienes listas todavía. Crea una!</h2>) :
-                    (<div className='contenedor-playlist-perfil'>
-                        {listas.map(lista => (
-                            lista && lista.id ? (
-                                <ListaGrande_Card key={lista.id} id={lista.id} titulo={lista.titulo} autor={username} descripcion={lista.descripcion}> </ListaGrande_Card>
-                            ) : null
-                        ))}
+            <div>
+                {listas.length === 0 ? (
+                    <h2>No tienes listas todavía. Crea una!</h2>
+                ) : listas.length === 1 ? (
+                    <div className='contenedor-playlist-perfil'>
+                        <ListaGrande_Card id={listas[0].id} titulo={listas[0].titulo} autor={username} descripcion={listas[0].descripcion} />
+                    </div>
+                ) : (
+                    <div className='contenedor-playlist-perfil'>
+                        <ListaGrande_Card id={listas[0].id} titulo={listas[0].titulo} autor={username} descripcion={listas[0].descripcion} />
+                        <ListaGrande_Card id={listas[1].id} titulo={listas[1].titulo} autor={username} descripcion={listas[1].descripcion} />
                     </div>
                 )}
             </div>
@@ -219,4 +194,9 @@ export const Perfil = () => {
     );
 };
 
-export default Perfil;
+export default PerfilId;
+
+
+
+
+
